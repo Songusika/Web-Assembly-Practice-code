2bb26084e9273ddbb745697d02d7a00af5c099d3
wooseok <sws981202@gmail.com>
2021-11-11 13:56:21 +0000

    add static file server

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const fs = require('fs');
 const static = require('serve-static');
 const app = new express();
 
+const static_file_path = "jang3"
+
 app.listen(3010, ()=> console.log("server started!")); //listen 메서드 서버 시작, 콜백함수는 시작할 떄, 호출
 
-app.use('/static', express.static(__dirname + '/unity_samples/test-time'));
+app.use('/static', express.static(__dirname + jang3));
