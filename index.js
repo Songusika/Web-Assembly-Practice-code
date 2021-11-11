bc76d2af5650202302001d217d56c9547ad07c3e
wooseok <sws981202@gmail.com>
2021-11-11 13:07:26 +0000

    Add static server

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const http = require('http');
+const fs = require('fs');
+const static = require('serve-static');
+const app = new express();
+
+app.listen(3010, ()=> console.log("server started!")); //listen 메서드 서버 시작, 콜백함수는 시작할 떄, 호출
+
+app.use('/static', express.static(__dirname + '/unity_samples/test-time'));
